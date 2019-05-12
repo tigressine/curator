10ea01355592cdcc6abada58ff59d41ec96edda4
Tiger Sachse <tiger@sachse.io>
2019-05-12 11:27:55 -0400

    comment drive

--- curator.py
+++ curator.py
@@ -1,4 +1,6 @@
-"""
+"""Curate images and videos with this simple script!
+
+Written by Tiger Sachse.
 """
 import shutil
 import pathlib
@@ -20,26 +22,33 @@ KNOWN_VIDEO_TYPES = {
     ".mov",
     ".wmv",
 }
-DEFAULT_TITLE = "New Album"
+DEFAULT_TITLE = "Album"
+DEFAULT_CLIPS_DIRECTORY = "Clips"
 TITLE_FORMAT = "{year}-{month} {title}"
 ITEM_FORMAT = "{initials}_{sequence}{extension}"
 
 
 class Parser(argparse.ArgumentParser):
-    """Class that handles command line arguments."""
+    """This class parses and sanitizes command line arguments."""
 
     def __init__(self, *args, **kwargs):
         """Initialize super and add some arguments."""
         super().__init__(*args, add_help=False, **kwargs)
         self.__add_arguments()
+
+    def parse_arguments(self):
+        """Parse the command line arguments."""
         self.__parse_arguments()
-        self.__validate_arguments()
+        self.__sanitize_arguments()
         if self.arguments["initials"] is None:
             self.__determine_initials()
         
+    def error(self, message):
+        """If any parsing error occurs, tell the user."""
+        print("An error occurred:", message)
 
     def __add_arguments(self):
-        """"""
+        """Add a handful of arguments to this script."""
         self.add_argument("destination")
         self.add_argument("sources", nargs="+")
         self.add_argument("--initials", default=None)
@@ -52,8 +61,8 @@ class Parser(argparse.ArgumentParser):
         self.arguments, self.unknown = self.parse_known_args()
         self.arguments = vars(self.arguments)
 
-    def __validate_arguments(self):
-        """"""
+    def __sanitize_arguments(self):
+        """Transform some arguments into more useful forms."""
         self.arguments["destination"] = pathlib.Path(self.arguments["destination"])
         self.arguments["sources"] = set(
             map(
@@ -61,15 +70,11 @@ class Parser(argparse.ArgumentParser):
                 self.arguments["sources"],
             ),
         )
-
-        self.arguments["month"] = int(self.arguments["month"])
-        self.arguments["year"] = int(self.arguments["year"])
-
-        # ensure that dest and src exist and are accessible
-        # sanitize title
+        self.arguments["month"] = str(self.arguments["month"]).rjust(2, "0")
+        self.arguments["year"] = str(self.arguments["year"]).rjust(4, "0")
 
     def __determine_initials(self):
-        """"""
+        """Get initials from the title."""
         self.arguments["initials"] = "".join(
             filter(
                 lambda character: character.isupper(),
@@ -77,46 +82,66 @@ class Parser(argparse.ArgumentParser):
             ),
         )
 
-    def error(self, message):
-        """If any parsing error occurs, tell the user."""
-        print("An error occurred:", message)
+
+def count_files(sources, desired_filetypes):
+    """Count the number of files in a collection of sources."""
+    file_count = 0
+    for source in sources:
+        for item in source.iterdir():
+            filetype = item.suffix.lower()
+            if filetype in desired_filetypes:
+                file_count += 1
+
+    return file_count
 
 
 # Main entry point of the script.
 parser = Parser()
+parser.parse_arguments()
 
-image_index = 0
-video_index = 0
+# Determine the number of places required for the image and video indexes.
+image_count = count_files(parser.arguments["sources"], KNOWN_IMAGE_TYPES)
+video_count = count_files(parser.arguments["sources"], KNOWN_VIDEO_TYPES)
+image_index_places = len(str(image_count))
+video_index_places = len(str(video_count))
+
+# Create the Path objects for the main destination and the video destination.
 destination = parser.arguments["destination"] / TITLE_FORMAT.format(
     year=parser.arguments["year"],
     month=parser.arguments["month"],
     title=parser.arguments["title"],
 )
-for source_directory in parser.arguments["sources"]:
-    for item in source_directory.iterdir():
-        lowercase_suffix = item.suffix.lower()
-        if lowercase_suffix in KNOWN_IMAGE_TYPES:
-            if not destination.exists():
-                destination.mkdir(parents=True)
+video_destination = destination / DEFAULT_CLIPS_DIRECTORY
+
+# If either destination doesn't exist, create it.
+if image_count > 0 and not destination.exists():
+    destination.mkdir(parents=True)
+if video_count > 0 and not video_destination.exists():
+    video_destination.mkdir(parents=True)
+
+# Copy each image and video into the correct destination.
+image_index = 1
+video_index = 1
+for source in parser.arguments["sources"]:
+    for item in source.iterdir():
+        filetype = item.suffix.lower()
+        if filetype in KNOWN_IMAGE_TYPES:
             shutil.copy2(
                 item,
                 destination / ITEM_FORMAT.format(
                     initials=parser.arguments["initials"],
-                    sequence=image_index,
-                    extension=lowercase_suffix,
+                    sequence=str(image_index).rjust(image_index_places, "0"),
+                    extension=filetype,
                 ),
             )
             image_index += 1
-        elif lowercase_suffix in KNOWN_VIDEO_TYPES:
-            video_destination = destination / "videos"
-            if not video_destination.exists():
-                video_destination.mkdir(parents=True)
+        elif filetype in KNOWN_VIDEO_TYPES:
             shutil.copy2(
                 item,
                 video_destination / ITEM_FORMAT.format(
                     initials=parser.arguments["initials"],
-                    sequence=video_index,
-                    extension=lowercase_suffix,
+                    sequence=str(video_index).rjust(video_index_places, "0"),
+                    extension=filetype,
                 ),
             )
             video_index += 1
